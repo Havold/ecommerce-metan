0270a06c5fdbed9474a829449aed240e163ffc48
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2026-03-15 15:32:00 +0700

    feat: create admin page.

---
+++ admin/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
+export default function HomePage() {
+  return <div className="">Hello</div>;
+}
---
+++ admin/components/Navbar.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  return (
+    <nav className="p-4 flex items-center justify-between">
+      <div className="">collapseButton</div>
+      <div className="flex gap-3 items-center">
+        <Link href="/dashboard">Dashboard</Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="transition-all rotate-0 dark:scale-0 dark:rotate-90" />
+              <Moon className="absolute transition-all scale-0 rotate-90 dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle Theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10} align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+              <span className="sr-only">Open menu</span>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10} align="end">
+            <DropdownMenuGroup>
+              <DropdownMenuLabel>Account</DropdownMenuLabel>
+              <DropdownMenuItem>
+                <User className="size-4 mr-2" />
+                Profile
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Settings className="size-4 mr-2" />
+                Settings
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem variant="destructive">
+                <LogOut className="size-4 mr-2" />
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
