e55dfa908d4cda66b1d0c1db3d943e626879717f
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2026-03-21 15:22:03 +0700

    feat: add component AppBarChart.

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { PlusCircle } from "lucide-react";
+import AppBarChar from "@/components/AppBarChar";
 
 export default function HomePage() {
-  return <div className="">Hello</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg md:col-span-2">
+        <h1 className="text-sm mb-4">Total Revenue</h1>
+        <AppBarChar />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg md:col-span-2">
+        Test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+    </div>
+  );
 }
