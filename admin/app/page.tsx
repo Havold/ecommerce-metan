cb24c92cee2ba359cfa9ef5db0df6985438046a2
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2026-03-21 20:25:18 +0700

    feat; Add component CardList.

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,16 +1,20 @@
 import AppAreaChar from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChar";
+import CardList from "@/components/CardList";
 import { TrendingUp } from "lucide-react";
 
 export default function HomePage() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
       <div className="bg-primary-foreground p-4 rounded-lg md:col-span-2">
         <h1 className="text-sm mb-8">Total Revenue</h1>
         <AppBarChart />
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <h1 className="text-sm mb-4">Latest Transactions</h1>
+        <CardList type="Latest Transactions" />
+      </div>
       <div className="bg-primary-foreground p-4 rounded-lg">
         <h1 className="text-sm mb-8">Browser Usage</h1>
         <AppPieChart />
@@ -29,7 +33,10 @@ export default function HomePage() {
         <h1 className="text-sm mb-8">Total Visitors</h1>
         <AppAreaChar />
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <h1 className="text-sm mb-4">Popular Content</h1>
+        <CardList type="Popular Content" />
+      </div>
     </div>
   );
 }
